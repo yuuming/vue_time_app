1a5794376d754eae99bc495ec141844603ef36f0
Yuumi <yuuming.n@gmail.com>
2018-12-27 23:55:43 +0900

    Modify placefolder and message

--- vue-cli-gihyo-example/src/components/HelloWorld.vue
+++ vue-cli-gihyo-example/src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <h1 v-if="['lucky person', 'happy person', 'funny person'].includes(who_i_am)">You have a glitter life!!! :)</h1>
-    <label for="person_textbox">I am a </label><input type="textbox" id="person_textbox" placeholder= "lucky person , happy person, funny person" v-model="who_i_am"><br>
-    <b v-if="!['person', 'cool person', 'minimalist'].includes(who_i_am)">You must be a lucky person, happy person, or funny person.</b>
+    <label for="person_textbox">I am a </label><input type="textbox" id="person_textbox" placeholder= "what person type you are" v-model="who_i_am"><br>
+    <b v-if="!['lucky person', 'happy person', 'funny person'].includes(who_i_am)">Please be happy and smile more!</b>
     <div v-if="who_i_am != 'minimalist'">
        <h2>Welcome to VuePractice page!</h2>
     <h3>Vue is very interesting</h3>
