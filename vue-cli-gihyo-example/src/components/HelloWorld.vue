45a5799a9c4767cadb204025633f524b72f37dfa
Yuumi <yuuming.n@gmail.com>
2018-12-27 23:38:22 +0900

    Add v-if for messages

--- vue-cli-gihyo-example/src/components/HelloWorld.vue
+++ vue-cli-gihyo-example/src/components/HelloWorld.vue
@@ -1,35 +1,25 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
-    <input type="checkbox" id="my_checkbox" v-model="checked">
-    <label for="my_checkbox">Check box.</label>
-    <h2>Welcome to VuePractice page!</h2>
+    <h1 v-if="['lucky person', 'happy person', 'funny person'].includes(who_i_am)">You have a glitter life!!! :)</h1>
+    <label for="person_textbox">I am a </label><input type="textbox" id="person_textbox" placeholder= "lucky person , happy person, funny person" v-model="who_i_am"><br>
+    <b v-if="!['person', 'cool person', 'minimalist'].includes(who_i_am)">You must be a lucky person, happy person, or funny person.</b>
+    <div v-if="who_i_am != 'minimalist'">
+       <h2>Welcome to VuePractice page!</h2>
     <h3>Vue is very interesting</h3>
+
+    </div>
   </div>
 </template>
-
-<style>
-.message { color: #a647dd; }
-</style>
-
+ 
 <script>
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Vue Practice Page!!!!',
-      checked: false
+      who_i_am: 'human'
     }
   },
   watch: {
-    checked: function (val) {
-      if (val) {
-        this.msg = 'Second message!'
-      }
-      else {
-        this.msg = 'First message!'
-      }
-    }
   }
 }
 </script>
