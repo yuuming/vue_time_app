039755363f4db8427424f6ccb31a109e1f727960
Yuumi <yuuming.n@gmail.com>
2018-12-27 22:17:07 +0900

    Add h1 and h2

--- vue-cli-gihyo-example/src/components/HelloWorld.vue
+++ vue-cli-gihyo-example/src/components/HelloWorld.vue
@@ -3,6 +3,8 @@
     <h1>{{ msg }}</h1>
     <input type="checkbox" id="my_checkbox" v-model="checked">
     <label for="my_checkbox">Check box.</label>
+    <h2>Welcome to VuePractice page!</h2>
+    <h3>Vue is very interesting</h3>
   </div>
 </template>
 
